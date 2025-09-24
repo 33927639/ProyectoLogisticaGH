4c5957b4812b660045f8673891d1efc55a19252a
Heydi Carrera <hsciajac@miumg.edu.gt>
2025-09-23 22:33:07 -0600

    vistas Heydi

--- app/Filament/Widgets/VentasMensuales.php
+++ app/Filament/Widgets/VentasMensuales.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\Venta;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -9,8 +10,15 @@ class VentasMensuales extends BaseWidget
 {
     protected function getStats(): array
     {
+        $totalVentas = Venta::whereMonth('created_at', now()->month)
+            ->whereYear('created_at', now()->year)
+            ->sum('monto');
+
         return [
-            //
+            Stat::make('Ventas del Mes', number_format($totalVentas, 2))
+                ->description('Total de ventas Q del mes actual')
+                ->color('success')
+                ->icon('heroicon-o-currency-dollar'),
         ];
     }
 }
