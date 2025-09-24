6e29ed9927f8559b30cb9eb40956e72a520182ca
Williams <weliasg@miumg.edu.gt>
2025-09-24 09:23:55 -0600

    Williams: Actualizacion de la vista ProductDescription.php

--- app/Filament/Resources/TblProductResource/Pages/ProductDescription.php
+++ app/Filament/Resources/TblProductResource/Pages/ProductDescription.php
@@ -4,6 +4,9 @@ namespace App\Filament\Resources\TblProductResource\Pages;
 
 use App\Filament\Resources\TblProductResource;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Infolists\Infolist;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
 
 class ProductDescription extends ViewRecord
 {
@@ -12,5 +15,22 @@ class ProductDescription extends ViewRecord
     public function getTitle(): string
     {
         return 'Descripción del Producto';
+    }
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist->schema([
+            Section::make('Descripción')
+                ->icon('heroicon-o-document-text')
+                ->description('Detalle completo del producto seleccionado')
+                ->schema([
+                    TextEntry::make('descripcion')
+                        ->label(false) // Oculta la etiqueta para que solo se muestre el texto
+                        ->markdown()   // Permite saltos de línea y formato si lo usas
+                        ->size('lg')
+                        ->color('gray'),
+                ])
+                ->columnSpanFull(),
+        ]);
     }
 }
