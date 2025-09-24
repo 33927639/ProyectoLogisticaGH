75cb8bac0e49364b3f7e426ca63650d0e6fc23fd
Heydi Carrera <hsciajac@miumg.edu.gt>
2025-09-23 22:29:10 -0600

    vistas Heydi

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReporteController;
 
@@ -11,3 +12,12 @@ Route::get('/api/ventas-mes-actual', [ReporteController::class, 'ventasMesActual
 
 // Ruta para la vista HTML
 Route::get('/reporte/ventas-mes', [ReporteController::class, 'mostrarReporteVentas'])->name('reporte.ventas.mes');
+
+// Ruta para reporte mensual (con lógica directa en la ruta)
+Route::get('/reporte-mensual', function () {
+    $totalVentas = \App\Models\Venta::whereMonth('created_at', now()->month)
+        ->whereYear('created_at', now()->year)
+        ->sum('monto');
+
+    return view('venta_mes', compact('totalVentas'));
+});
